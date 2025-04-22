05ac88510277a2393105516358b05b61f7b694b0
Zarni Hlawn <zarnihlawn@outlook.com>
2025-04-22 16:43:53 +0630

    lgtm

--- src/lib/component/local/app/workspace-dashboard/component/QueryBlockActionsCodingWorkspace.svelte
+++ src/lib/component/local/app/workspace-dashboard/component/QueryBlockActionsCodingWorkspace.svelte
@@ -1,23 +1,34 @@
 <script lang="ts">
 	import type { ContentTypeInterface } from '$lib/model/interface/schema.interface';
+	import QueryBlockCodingWorkspace from './QueryBlockCodingWorkspace.svelte';
 
 	let { contentType } = $props<{ contentType: ContentTypeInterface[] }>();
+
+	let codingBlockCreated = $state(false);
 </script>
 
 <main class="bg-base-200 rounded-box my-3 flex justify-between p-3 shadow-sm">
 	<section>
-		<div
-			class="tooltip tooltip-right tooltip-primary"
-			data-tip="create a new coding block"
-		>
-			<button class="btn btn-primary"> New </button>
-		</div>
-		<div
-			class="tooltip tooltip-right tooltip-success"
-			data-tip="execute this query block"
-		>
-			<button class="btn btn-success"> Run </button>
-		</div>
+		{#if !codingBlockCreated}
+			<div
+				class="tooltip tooltip-right tooltip-primary"
+				data-tip="create a new coding block"
+			>
+				<button
+					class="btn btn-primary"
+					onclick={() => (codingBlockCreated = true)}
+				>
+					New
+				</button>
+			</div>
+		{:else}
+			<div
+				class="tooltip tooltip-right tooltip-success"
+				data-tip="execute this query block"
+			>
+				<button class="btn btn-success"> Run </button>
+			</div>
+		{/if}
 	</section>
 	<section>
 		<select class="select mx-2">
@@ -29,11 +40,24 @@
 	</section>
 
 	<section>
-		<div
-			class="tooltip tooltip-left tooltip-error"
-			data-tip="delete this coding block"
-		>
-			<button class="btn btn-error"> Del </button>
-		</div>
+		{#if codingBlockCreated}
+			<div
+				class="tooltip tooltip-left tooltip-warning"
+				data-tip="edit this coding block"
+			>
+				<button
+					class="btn btn-warning"
+					onclick={() => (codingBlockCreated = false)}
+				>
+					Del
+				</button>
+			</div>
+		{/if}
 	</section>
+
+	{#if codingBlockCreated}
+		<section>
+			<QueryBlockCodingWorkspace />
+		</section>
+	{/if}
 </main>
